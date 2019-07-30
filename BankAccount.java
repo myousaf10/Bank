56f80b64b8661bd4eab5ec159ae11cacd5ece027
myousaf10 <53448267+myousaf10@users.noreply.github.com>
2019-07-30 12:52:00 -0700

    Add files via upload
    Implemented all the initial requirments

---
+++ BankAccount.java
@@ -0,0 +1,43 @@
+package july_29;
+
+public class BankAccount {
+		private int account_number = 123456789;
+		private double balance = 50.0;
+		private String customer_name = "Yousaf";
+		private String	email = "Yousaf@gmail.com";
+		private int phone_number = 123456789;
+		
+		public void set_balance(double new_amount) {
+			
+			
+			balance=balance+new_amount;
+			System.out.println("Available Balance " + balance);
+		}
+		
+		public void withdraw(double withdraw_amount) {
+			if(balance>0 && (balance-withdraw_amount)>0)
+			{
+			balance=balance-withdraw_amount;
+			System.out.println("Customer Name " + customer_name);
+			System.out.println("Available Balance " + balance);
+			}
+			else 
+				System.out.println("Can not Perform the Transaction");
+		}
+		
+		public void print() {
+			System.out.println("\nCustomer Name: " + customer_name);
+			System.out.println("Account Number: " + account_number);
+			System.out.println("Available Balance: " + balance);
+			System.out.println("Email ID Registered to the user: " + email);
+			System.out.println("Phone Number Registered to the user: " + phone_number);
+			
+		}
+		
+		public double get_balance() {
+			return this.balance;
+		}
+		
+		
+		
+}
